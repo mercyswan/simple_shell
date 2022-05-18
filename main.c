ba7600dac3622453d8d51f5ae4b3980c71cc5b97
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-05-18 18:23:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
-binbash
+#include "shell.h"
+
+/**
+ * main - function that checks if our Shell is called
+ * in interactive or non-interactive mode.
+ * @ac: number of arguments passed (non-interactive)
+ * @av: double pointer with arguments passed (non-interactive)
+ * Return: 0 on success.
+ */
+
+int main(int ac, char **av)
+{
+	if (isatty(STDIN_FILENO) == 1)
+		shell_interactive();
+	else
+		shell_not_interactive(ac, av);
+
+	return (0);
+}
