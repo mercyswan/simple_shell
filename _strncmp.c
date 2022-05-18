f637b4fe37ce582acae74cdfa147c97acf195518
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-05-18 20:31:28 +0100

    Update _strncmp.c

--- _strncmp.c
+++ _strncmp.c
@@ -1,1 +1,27 @@
-binbash
+#include "shell.h"
+
+/**
+* _strncmp - function that compares two strings.
+* @path: string 1 to compare.
+* @match: string 2 to compare.
+* @n: number of characteres to take into account.
+* Return: 0 if strings are equal, 1 if strings are different.
+*/
+
+char *_strncmp(char *path, char *match, size_t n)
+{
+	while (n && *path && (*path == *match))
+	{
+		++path;
+		++match;
+		--n;
+	}
+	if (n == 0)
+	{
+		return (path);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
